251c0780f646bc608e46d2e7f66ba1af080fad8f
rmuscaliuc <muscaliucrobert@gmail.com>
2025-07-17 11:15:03 +0300

    first commit

---
+++ be-a-volunteer/src/main/java/com/example/be_a_volunteer/BeAVolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_a_volunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAVolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAVolunteerApplication.class, args);
+	}
+
+}
---
+++ be-a-volunteer/src/test/java/com/example/be_a_volunteer/BeAVolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_a_volunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAVolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
